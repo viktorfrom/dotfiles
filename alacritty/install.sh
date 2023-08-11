ef09e97761e4668c1af5e99f1e1a25f26dc2c4e7
viktorfrom <viktor.from91@gmail.com>
2023-08-11 13:18:01 +0200

    Add alacritty, tmux, zsh conf

---
+++ alacritty/install.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt upgrade
+sudo add-apt-repository ppa:aslatter/ppa -y
+sudo apt install alacritty
---
+++ zsh/install.sh
@@ -0,0 +1,3 @@
+sudo apt update && sudo apt upgrade 
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
